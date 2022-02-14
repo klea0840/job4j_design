8446cc332f84d8637fd440f24b642b47c06eda98
klea0840 <klea0840@gmail.com>
2022-02-14 18:01:21 +0300

    5.2. Архивировать проект [#861] corr3

--- src/main/java/io/Zip.java
+++ src/main/java/io/Zip.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.zip.ZipEntry;
@@ -13,21 +14,31 @@ import java.util.zip.ZipOutputStream;
 import static java.util.Objects.isNull;
 
 public class Zip {
-    public static void packFiles(List<File> sources, File target) {
-        if (!target.exists()) {
-            throw new IllegalArgumentException("File does not exist");
-        }
+    public static void packFiles(List<File> sources, File target, String parentDir) {
+
         if (isNull(sources)) {
             throw new IllegalArgumentException("Source does not exist");
         }
 
+        String entryPath;
+        System.out.println("Zipping files");
         try (ZipOutputStream zips = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            for (var file
-                    :
-                 sources) {
-            zips.putNextEntry(new ZipEntry(file.getPath()));
-            try (BufferedInputStream outs = new BufferedInputStream(new FileInputStream(file))) {
-                zips.write(outs.readAllBytes());
+            for (var file: sources) {
+
+                // Находим относительный путь (путь к файлу относительно родительской папки)
+                // Необходимо для корректной архивации
+                if (file.getPath().startsWith(parentDir)) {
+                    entryPath = file.getPath().substring(parentDir.length());
+                } else {
+                    entryPath = file.getPath();
+                }
+
+                zips.putNextEntry(new ZipEntry(entryPath));
+                try (BufferedInputStream outs = new BufferedInputStream(new FileInputStream(file))) {
+                    zips.write(outs.readAllBytes());
+                    zips.closeEntry();
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
         } catch (Exception e) {
@@ -50,11 +61,10 @@ public class Zip {
         ArgsName check = ArgsName.of(args);
         System.out.println(check.get("d"));
         System.out.println(check.get("o"));
-        String[] end = args[1].split("\\.");
-        System.out.println(end[1]);
-        Predicate<Path> condition = p -> p.toFile().getName().endsWith(end[1]);
+//        String[] end = check.get("e").split("\\.");
+//        System.out.println(Arrays.toString(end));
+        Predicate<Path> condition = p -> p.toFile().getName().endsWith(check.get("e"));
         Predicate<Path> noNcondition = condition.negate();
-
 
         List<Path> sources = new ArrayList<>();
 
@@ -65,12 +75,15 @@ public class Zip {
             e.printStackTrace();
         }
 
+        System.out.println("sources: " + sources);
+
         List<File> files = new ArrayList<>();
         for (var path: sources) {
             files.add(path.toFile());
         }
         File target = new File(check.get("o"));
 //
-        packFiles(files, target);
+        packFiles(files, target, check.get("d"));
+//        packSingleFile(new File("pom.xml"), new File("output111.zip"));
     }
 }
